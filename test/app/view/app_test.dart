1881492de3790f4474cd7b48289f8832ed6897cb
NonymousMorlock <agamspaul@gmail.com>
2024-01-10 08:28:09 +0000

    Automated Test: App Test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,18 @@
 import 'package:conversation_log/app/app.dart';
-import 'package:conversation_log/counter/counter.dart';
+import 'package:conversation_log/core/services/injection_container.dart';
+import 'package:conversation_log/src/home/presentation/views/home_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
+  setUp(() async {
+    SharedPreferences.setMockInitialValues({});
+    await init();
+  });
   group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
+    testWidgets('renders HomeScreen', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomeScreen), findsOneWidget);
     });
   });
 }
