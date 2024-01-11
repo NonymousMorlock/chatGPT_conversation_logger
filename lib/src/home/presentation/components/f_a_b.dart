899c06426d482c9dece584ceae4b82251dd299c8
NonymousMorlock <agamspaul@gmail.com>
2024-01-11 12:06:15 +0000

    Theming and Hover detection style change

--- lib/src/home/presentation/components/f_a_b.dart
+++ lib/src/home/presentation/components/f_a_b.dart
@@ -1,7 +1,9 @@
+import 'package:conversation_log/core/common/theme/cubit/theme_cubit.dart';
 import 'package:conversation_log/src/home/data/models/conversation_model.dart';
 import 'package:conversation_log/src/home/presentation/app/bloc/conversation_bloc.dart';
 import 'package:conversation_log/src/home/presentation/app/providers/home_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 
 class FAB extends StatefulWidget {
@@ -18,50 +20,56 @@ class _FABState extends State<FAB> {
   Widget build(BuildContext context) {
     return Consumer<HomeProvider>(
       builder: (_, provider, __) {
-        return MouseRegion(
-          onEnter: (_) {
-            Future.delayed(
-              const Duration(milliseconds: 500),
-              () {
-                if (!mounted) return;
-                setState(() => extend = true);
+        return BlocBuilder<ThemeCubit, ThemeState>(
+          builder: (context, state) {
+            return MouseRegion(
+              onHover: (_) {
+                Future.delayed(
+                  const Duration(milliseconds: 500),
+                  () {
+                    if (!mounted) return;
+                    setState(() => extend = true);
+                  },
+                );
               },
+              onExit: (_) {
+                Future.delayed(
+                  const Duration(milliseconds: 500),
+                  () {
+                    if (!mounted) return;
+                    setState(() => extend = false);
+                  },
+                );
+              },
+              child: extend
+                  ? FloatingActionButton.extended(
+                      backgroundColor: state.accentColor,
+                      onPressed: onPressed,
+                      label: Text(
+                        (provider.canSaveMessage || provider.isEnteringTitle)
+                            ? 'Save'
+                            : 'New '
+                                'Conversation',
+                      ),
+                      icon: Icon(
+                        (provider.canSaveMessage || provider.isEnteringTitle)
+                            ? Icons.save
+                            : Icons.add,
+                      ),
+                      elevation: 19,
+                    )
+                  : FloatingActionButton(
+                      backgroundColor: state.accentColor,
+                      onPressed: onPressed,
+                      elevation: 19,
+                      child: Icon(
+                        (provider.canSaveMessage || provider.isEnteringTitle)
+                            ? Icons.save
+                            : Icons.add,
+                      ),
+                    ),
             );
           },
-          onExit: (_) {
-            Future.delayed(
-              const Duration(milliseconds: 500),
-              () {
-                if (!mounted) return;
-                setState(() => extend = false);
-              },
-            );
-          },
-          child: extend
-              ? FloatingActionButton.extended(
-                  onPressed: onPressed,
-                  label: Text(
-                    (provider.canSaveMessage || provider.isEnteringTitle)
-                        ? 'Save'
-                        : 'New '
-                            'Conversation',
-                  ),
-                  icon: Icon(
-                    (provider.canSaveMessage || provider.isEnteringTitle)
-                        ? Icons.save
-                        : Icons.add,
-                  ),
-                  elevation: 19,
-                )
-              : FloatingActionButton(
-                  onPressed: onPressed,
-                  elevation: 19,
-                  child: Icon(
-                    (provider.canSaveMessage || provider.isEnteringTitle)
-                        ? Icons.save
-                        : Icons.add,
-                  ),
-                ),
         );
       },
     );
