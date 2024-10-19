121e65a0b51496d8c044a2c19bb8fd58f9ae8bbd
NonymousMorlock <agamspaul@gmail.com>
2024-10-19 21:02:25 +0000

    🧹 Refactor: Update MarkdownRenderer to support code block wrapping and custom text node
    - Update MarkdownRenderer to extend StatefulWidget instead of StatelessWidget
    - Add CodeBlockWrapper widget import
    - Add MarkdownCustom imports for custom text node and latex syntax
    - Update MarkdownWidget import
    - Add TocController and dispose method to _MarkdownRendererState
    - Update build method to use BlocBuilder for theme state
    - Update config and theme based on theme state
    - Add CodeBlockWrapper function to wrap code blocks
    - Update theme data and text style for MarkdownWidget
    - Update MarkdownConfig based on theme state
    - Update MarkdownWidget parameters with widget properties
    - Add tocController and markdownGenerator parameters to MarkdownWidget
    - Add generators and inlineSyntaxList to markdownGenerator
    - Add CustomTextNode and richTextBuilder to markdownGenerator

--- lib/src/home/presentation/widgets/markdown_renderer.dart
+++ lib/src/home/presentation/widgets/markdown_renderer.dart
@@ -1,11 +1,12 @@
 import 'package:conversation_log/core/common/theme/cubit/theme_cubit.dart';
+import 'package:conversation_log/src/home/presentation/widgets/code_block_wrapper.dart';
+import 'package:conversation_log/src/home/presentation/widgets/markdown_custom/custom_text_node.dart';
+import 'package:conversation_log/src/home/presentation/widgets/markdown_custom/latex_syntax.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:markdown_widget/config/configs.dart';
-import 'package:markdown_widget/widget/blocks/leaf/paragraph.dart';
-import 'package:markdown_widget/widget/markdown.dart';
+import 'package:markdown_widget/markdown_widget.dart';
 
-class MarkdownRenderer extends StatelessWidget {
+class MarkdownRenderer extends StatefulWidget {
   const MarkdownRenderer(
     this.data, {
     super.key,
@@ -18,39 +19,70 @@ class MarkdownRenderer extends StatelessWidget {
   final bool shrinkWrap;
 
   @override
+  State<MarkdownRenderer> createState() => _MarkdownRendererState();
+}
+
+class _MarkdownRendererState extends State<MarkdownRenderer> {
+  final TocController controller = TocController();
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return BlocBuilder<ThemeCubit, ThemeState>(
       builder: (context, state) {
-        return MarkdownWidget(
-          data: data,
-          physics: physics,
-          shrinkWrap: shrinkWrap,
-          config: state is ThemeStateDark
-              ? MarkdownConfig(
-                  configs: [
-                    PConfig(
-                      textStyle: MarkdownConfig.darkConfig.p.textStyle.copyWith(
-                        color: Colors.white,
-                      ),
-                    ),
-                    MarkdownConfig.darkConfig.a,
-                    MarkdownConfig.darkConfig.blockquote,
-                    MarkdownConfig.darkConfig.code,
-                    MarkdownConfig.darkConfig.h1,
-                    MarkdownConfig.darkConfig.h2,
-                    MarkdownConfig.darkConfig.h3,
-                    MarkdownConfig.darkConfig.h4,
-                    MarkdownConfig.darkConfig.h5,
-                    MarkdownConfig.darkConfig.h6,
-                    MarkdownConfig.darkConfig.hr,
-                    MarkdownConfig.darkConfig.img,
-                    MarkdownConfig.darkConfig.li,
-                    MarkdownConfig.darkConfig.input,
-                    MarkdownConfig.darkConfig.pre,
-                    MarkdownConfig.darkConfig.table,
-                  ],
-                )
-              : MarkdownConfig.defaultConfig,
+        final isDark = state is DarkTheme;
+        final config =
+            isDark ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
+        CodeBlockWrapper codeWrapper(
+          Widget child,
+          String text,
+          String language,
+        ) {
+          return CodeBlockWrapper(child, text, language);
+        }
+
+        final theme = Theme.of(context);
+        return Theme(
+          data: theme.copyWith(
+            textTheme: theme.textTheme.copyWith(
+              titleLarge: theme.textTheme.titleLarge?.copyWith(
+                color: state.primaryTextColor,
+              ),
+            ),
+          ),
+          child: DefaultTextStyle.merge(
+            style: TextStyle(color: state.primaryTextColor),
+            child: MarkdownWidget(
+              data: widget.data,
+              physics: widget.physics,
+              shrinkWrap: widget.shrinkWrap,
+              config: config.copy(
+                configs: [
+                  if (isDark)
+                    PreConfig.darkConfig.copy(wrapper: codeWrapper)
+                  else
+                    const PreConfig().copy(wrapper: codeWrapper),
+                ],
+              ),
+              tocController: controller,
+              markdownGenerator: MarkdownGenerator(
+                generators: [latexGenerator],
+                inlineSyntaxList: [LatexSyntax()],
+                textGenerator: (node, config, visitor) {
+                  return CustomTextNode(node.textContent, config, visitor);
+                },
+                richTextBuilder: (span) => Text.rich(
+                  span,
+                  textScaler: TextScaler.noScaling,
+                ),
+              ),
+            ),
+          ),
         );
       },
     );
